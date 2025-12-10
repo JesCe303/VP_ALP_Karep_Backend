7e44b2a5bb8f32563509c6b5ad420d839a1db2ff
chris <christoperjoel1@gmail.com>
2025-12-10 18:30:06 +0700

    Migrated (tulis di terminal `npx prisma migrate deploy ` & `npx prisma generate`)

---
+++ prisma/migrations/20251210111951_alp_vp/migration.sql
@@ -0,0 +1,153 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "user_id" SERIAL NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "address" VARCHAR(255),
+    "phone_number" VARCHAR(20),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("user_id")
+);
+
+-- CreateTable
+CREATE TABLE "notifications" (
+    "notification_id" SERIAL NOT NULL,
+    "title" VARCHAR(100) NOT NULL,
+    "subtitle" TEXT,
+    "user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "notifications_pkey" PRIMARY KEY ("notification_id")
+);
+
+-- CreateTable
+CREATE TABLE "experiences" (
+    "experience_id" SERIAL NOT NULL,
+    "title" VARCHAR(100) NOT NULL,
+    "description" TEXT,
+    "user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "experiences_pkey" PRIMARY KEY ("experience_id")
+);
+
+-- CreateTable
+CREATE TABLE "achievements" (
+    "achievement_id" SERIAL NOT NULL,
+    "title" VARCHAR(100) NOT NULL,
+    "description" TEXT,
+    "user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "achievements_pkey" PRIMARY KEY ("achievement_id")
+);
+
+-- CreateTable
+CREATE TABLE "company" (
+    "company_id" SERIAL NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "address" VARCHAR(255),
+    "phone_number" VARCHAR(20),
+    "website" VARCHAR(255),
+    "vision_mission" TEXT,
+    "description" TEXT,
+    "founding_date" DATE,
+    "logo_path" VARCHAR(255),
+    "image_path" VARCHAR(255),
+    "user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "company_pkey" PRIMARY KEY ("company_id")
+);
+
+-- CreateTable
+CREATE TABLE "company_to_tag" (
+    "company_id" INTEGER NOT NULL,
+    "company_tag_id" INTEGER NOT NULL,
+
+    CONSTRAINT "company_to_tag_pkey" PRIMARY KEY ("company_id","company_tag_id")
+);
+
+-- CreateTable
+CREATE TABLE "company_tag" (
+    "company_tag_id" SERIAL NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+
+    CONSTRAINT "company_tag_pkey" PRIMARY KEY ("company_tag_id")
+);
+
+-- CreateTable
+CREATE TABLE "jobs" (
+    "job_id" SERIAL NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "description" TEXT,
+    "company_id" INTEGER NOT NULL,
+
+    CONSTRAINT "jobs_pkey" PRIMARY KEY ("job_id")
+);
+
+-- CreateTable
+CREATE TABLE "job_to_tag" (
+    "job_id" INTEGER NOT NULL,
+    "tag_id" INTEGER NOT NULL,
+
+    CONSTRAINT "job_to_tag_pkey" PRIMARY KEY ("job_id","tag_id")
+);
+
+-- CreateTable
+CREATE TABLE "job_tag" (
+    "job_tag_id" SERIAL NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+
+    CONSTRAINT "job_tag_pkey" PRIMARY KEY ("job_tag_id")
+);
+
+-- CreateTable
+CREATE TABLE "applications" (
+    "application_id" SERIAL NOT NULL,
+    "status" VARCHAR(20) NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "job_id" INTEGER NOT NULL,
+
+    CONSTRAINT "applications_pkey" PRIMARY KEY ("application_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "company_email_key" ON "company"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "company_user_id_key" ON "company"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "notifications" ADD CONSTRAINT "notifications_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("user_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "experiences" ADD CONSTRAINT "experiences_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("user_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "achievements" ADD CONSTRAINT "achievements_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("user_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "company" ADD CONSTRAINT "company_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("user_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "company_to_tag" ADD CONSTRAINT "company_to_tag_company_id_fkey" FOREIGN KEY ("company_id") REFERENCES "company"("company_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "company_to_tag" ADD CONSTRAINT "company_to_tag_company_tag_id_fkey" FOREIGN KEY ("company_tag_id") REFERENCES "company_tag"("company_tag_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "jobs" ADD CONSTRAINT "jobs_company_id_fkey" FOREIGN KEY ("company_id") REFERENCES "company"("company_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "job_to_tag" ADD CONSTRAINT "job_to_tag_job_id_fkey" FOREIGN KEY ("job_id") REFERENCES "jobs"("job_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "job_to_tag" ADD CONSTRAINT "job_to_tag_tag_id_fkey" FOREIGN KEY ("tag_id") REFERENCES "job_tag"("job_tag_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "applications" ADD CONSTRAINT "applications_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("user_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "applications" ADD CONSTRAINT "applications_job_id_fkey" FOREIGN KEY ("job_id") REFERENCES "jobs"("job_id") ON DELETE CASCADE ON UPDATE CASCADE;
