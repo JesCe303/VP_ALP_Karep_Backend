c782de53280a8074df48ad76680905223f12bf77
chris <christoperjoel1@gmail.com>
2025-12-11 20:48:12 +0700

    Router Application

--- src/route/appRoute/application-route.ts
+++ src/route/appRoute/application-route.ts
@@ -1,10 +1,11 @@
 import express from "express"
 import { authMiddleware } from "../../middleware/auth-middleware";
+import { ApplicationController } from "../../controller/appController/application-controller";
 
 export const appRouter = express.Router();
 
 appRouter.use(authMiddleware)
 
-appRouter.get("/application-list")
-appRouter.post("/application/:jobId")
-appRouter.patch("/application/:idApp")
+appRouter.get("/application-list", ApplicationController.getMyApplication)
+appRouter.post("/application/:jobId", ApplicationController.hiringApplication)
+appRouter.patch("/application/:idApp", ApplicationController.cancelApplication)
