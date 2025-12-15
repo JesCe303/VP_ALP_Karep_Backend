0fb1083ade09a3a2bf896c6b29d1c939cd6788e3
Angga Dhammika Jaya <anggadj94@gmail.com>
2025-12-15 16:37:50 +0700

    Create companyTag-controller.ts

---
+++ src/controller/companyTag-controller.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Response } from "express";
+import { CompanyTagService } from "../services/companyTag-service";
+
+export class CompanyTagController {
+    static async getAllCompanyTags(
+        req: UserRequest,
+        res: Response,
+        next: NextFunction
+    ) {
+        try {
+            const response = await CompanyTagService.getAllCompanyTags();
+
+            res.status(200).json({
+                data: response
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
+}
