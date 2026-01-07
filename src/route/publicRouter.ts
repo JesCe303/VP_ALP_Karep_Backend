f6046caef88f6faec776cbc2fee3a9450f0c3db3
chris <christoperjoel1@gmail.com>
2026-01-07 12:08:51 +0700

    Error Fix

--- src/route/publicRouter.ts
+++ src/route/publicRouter.ts
@@ -3,8 +3,8 @@
 import express from "express";
 import { LoginDevController } from "../auth/login-dev";
 
-export const publicRouter = express.Router();
+export const publicRouterFakeLog = express.Router();
 
-publicRouter.post("/loginDev", LoginDevController.loginDev);
-publicRouter.post("/loginUser", LoginDevController.loginDevUser)
-publicRouter.post("/createCompany", LoginDevController.companyConnect)
+publicRouterFakeLog.post("/loginDev", LoginDevController.loginDev);
+publicRouterFakeLog.post("/loginUser", LoginDevController.loginDevUser)
+publicRouterFakeLog.post("/createCompany", LoginDevController.companyConnect)
